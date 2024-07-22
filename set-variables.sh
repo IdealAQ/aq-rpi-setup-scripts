63d82254df8015bb693b530012be2473b7cdb537
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-07-22 17:16:06 +0300

    set variables 2

--- set-variables.sh
+++ set-variables.sh
@@ -3,6 +3,6 @@
 echo "Please enter the PORT to be used for FRP:"
 read PORT
 
-echo "PORT: , $PORT.
+echo "PORT: , $PORT."
 
-echo 'export AQ_FRP_PORT=$PORT' >> ~/.bashrc
+echo "export AQ_FRP_PORT=\"$PORT\"" >> ~/.bashrc

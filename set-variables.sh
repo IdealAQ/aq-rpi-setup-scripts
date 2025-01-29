e99d27df9704296215bc231c58453cc83bd27634
drohal3 <rohal.dominik.123@gmail.com>
2025-01-29 22:58:11 +0200

    added variables

--- set-variables.sh
+++ set-variables.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 variables=(
-  "AQ_FRP_PORT,Port to be used for FRP"
   "AQ_DEVICE_ID,Device identifier"
+  "AQ_FRP_SERVER_ADDRESS,FRP server address"
+  "AQ_FRP_SERVER_PORT,FRP server port"
+  "AQ_FRP_DEVICE_PORT,FRP device port"
 )
 
 echo "verifying if all variables are set..."
 echo ""
-printf "%-20s %-15s %-10s \n" "Name" "Value" "Description"
+printf "%-25s %-15s %-10s \n" "Name" "Value" "Description"
 echo "---------------------------------------------------------------------"
 missing=()
 
@@ -15,10 +17,10 @@ for index in "${!variables[@]}"; do
   variable="${variables[$index]}"
   IFS=',' read -r name description <<< "$variable"
   if [ -z "${!name}" ]; then # check if variable is set already
-    missing+=$index
-    printf "%-20s %-15s %-10s \n" "${name}" "< MISSING!!! >" "${description}"
+    missing+=($index)
+    printf "%-25s %-15s %-10s \n" "${name}" "< MISSING!!! >" "${description}"
   else
-    printf "%-20s %-15s %-10s \n" "${name}" "${!name}" "${description}"
+    printf "%-25s %-15s %-10s \n" "${name}" "${!name}" "${description}"
   fi
 done
 
@@ -36,8 +38,7 @@ echo ""
 for missing_index in "${missing}"; do
   missing_variable="${variables[$missing_index]}"
   IFS=',' read -r name description <<< "$missing_variable"
-
-  echo "Please, enter ${name} (${description})"
+  echo "Please, enter ${name} (${description}):"
   read value
   echo "export ${name}=\"$value\"" >> ~/.bashrc
 done

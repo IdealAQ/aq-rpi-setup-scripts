fdb95ea55fe0f6050d1a334cd44f2b0d6ead0c0f
drohal3 <rohal.dominik.123@gmail.com>
2025-01-30 19:47:49 +0200

    check if frpc service exist

--- frp-setup.sh
+++ frp-setup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+echo "Checking if frpc.service exists..."
+if systemctl list-units --type=service --all | grep -q "frpc.service"; then
+    echo "frpc.service exists."
+    
+    # Check if the service is running
+    if systemctl is-active --quiet frpc.service; then
+        echo "frpc.service is running."
+        echo "Exiting the script!"
+        echo "No change has been performed."
+    else
+        echo "frpc.service is NOT running."
+        echo "to start the service, run \"sudo systemctl start frpc.service\""
+    fi
+    return
+else
+    echo "frpc.service does not exist."
+    echo ""
+    echo "Continuing with setting up the service..."
+fi
+
 . ./set-variables.sh
 
 envsubst < ~/aq/scripts/templates/template_etc_frpc.ini > just_testing.conf
 
+cd /tmp
+wget https://github.com/fatedier/frp/releases/download/v0.49.0/frp_0.49.0_linux_amd64.tar.gz
+tar -xzf frp_0.49.0_linux_amd64.tar.gz

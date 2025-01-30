030e4c371bf29e97768a488c55a4e9e08996da9d
drohal3 <rohal.dominik.123@gmail.com>
2025-01-30 16:03:50 +0200

    updated set-variables

--- README.md
+++ README.md
@@ -20,12 +20,18 @@ This will pull scripts in this repository to the file
 Checks if the variables required to set up FRP remote access are set.
 If some variables are not set, it prompts their values.
 
+",,aq"
+  "AQ_USER,Group of user to run FRP service,aq"
+
 | variable              | description        |
 |-----------------------|--------------------|
 | AQ_DEVICE_ID          | Device identifier  |
 | AQ_FRP_SERVER_ADDRESS | FRP server address |
-| AQ_FRP_SERVER_PORT    | FRP server port    |
-| AQ_FRP_DEVICE_PORT    | FRP device port    |
+| AQ_FRP_SERVER_PORT    | FRP server port     |
+| AQ_FRP_DEVICE_PORT    | FRP device port     |
+| AQ_USER    | User to run FRP service     |
+| AQ_GROUP    | Group of user to run FRP service     |
 
-> to update already set variable, you must manually remove the line with the variable
-> by editing ~/.bashrc file, opening new terminal and rerunning the script.
+
+> to update already set variable, you must manually remove the variable
+> by editing ~/.bashrc file, rebooting `sudo reboot` and rerunning the script.
--- set-variables.sh
+++ set-variables.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 variables=(
-  "AQ_DEVICE_ID,Device identifier"
-  "AQ_FRP_SERVER_ADDRESS,FRP server address"
-  "AQ_FRP_SERVER_PORT,FRP server port"
-  "AQ_FRP_DEVICE_PORT,FRP device port"
+  "AQ_DEVICE_ID,Device identifier,"
+  "AQ_FRP_SERVER_ADDRESS,FRP server address,"
+  "AQ_FRP_SERVER_PORT,FRP server port,7000"
+  "AQ_FRP_DEVICE_PORT,FRP device port,"
+  "AQ_USER,User to run FRP service,aq"
+  "AQ_GROUP,Group of user to run FRP service,aq"
+
 )
 
 echo "verifying if all variables are set..."
@@ -15,7 +18,7 @@ missing=()
 
 for index in "${!variables[@]}"; do
   variable="${variables[$index]}"
-  IFS=',' read -r name description <<< "$variable"
+  IFS=',' read -r name description default <<< "$variable"
   if [ -z "${!name}" ]; then # check if variable is set already
     missing+=($index)
     printf "%-25s %-15s %-10s \n" "${name}" "< MISSING!!! >" "${description}"
@@ -28,6 +31,8 @@ echo ""
 
 if [ ${#missing[@]} -eq 0 ]; then
   echo "All variables are set!"
+  echo "For instructions to edit already set variable, navigate to the documentation:"
+  echo ">>> https://github.com/IdealAQ/aq-rpi-setup-scripts"
   return
 fi
 
@@ -36,11 +41,29 @@ echo "Set (${#missing[@]}) missing variables..."
 echo ""
 
 for missing_index in "${missing[@]}"; do
+  success=0
+  unset default
   missing_variable="${variables[$missing_index]}"
-  IFS=',' read -r name description <<< "$missing_variable"
-  echo "Please, enter ${name} (${description}):"
-  read value
-  echo "export ${name}=\"$value\"" >> ~/.bashrc
+  IFS=',' read -r name description default <<< "$missing_variable"
+  
+  while [ "$success" -eq 0 ]; do
+    echo "Setting variable ${name} (${description})"
+    if [ ! -z "$default" ]; then
+      echo "Press ENTER to use default value \"${default}\"."
+    fi
+    read -p "provide value: " value
+    if [ -z "$value" ]; then
+      if [ ! -z "$default" ]; then
+        value="$default"
+      fi
+    fi
+    
+    if [ ! -z "$value" ]; then
+      echo "Setting value \"${value}\" for variable ${name}."
+      echo "export ${name}=\"$value\"" >> ~/.bashrc
+      success=1
+    fi
+  done
 done
 
 source ~/.bashrc

c7050c83989d30b724cb20ff775847e7c4e08ae1
Dominik Rohal <rohal.dominik.123@gmail.com>
2026-07-13 15:41:43 +0300

    fixed cvat-init

--- cvat/cvat-init.sh
+++ cvat/cvat-init.sh
@@ -13,12 +13,18 @@ sudo sh get-docker.sh
 
 sudo usermod -aG docker cvatadmin
 
-git clone https://github.com/cvat-ai/cvat.git /home/cvatadmin/cvat
+git clone --branch v2.69.0 https://github.com/cvat-ai/cvat.git /home/cvatadmin/cvat
 
 sudo chown -R cvatadmin:cvatadmin /home/cvatadmin/cvat
 
 cd /home/cvatadmin/cvat
 
-export CVAT_HOST="$1"
+touch .env
+
+if grep -q '^CVAT_HOST=' .env; then
+    sed -i "s/^CVAT_HOST=.*/CVAT_HOST=$1/" .env
+else
+    echo "CVAT_HOST=$1" >> .env
+fi
 
 sudo -u cvatadmin docker compose up -d
